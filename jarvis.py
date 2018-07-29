f2088a7cff573d422ef266440f4cd896823114ed
jshuadvd <jshuadvd@icloud.com>
2018-07-29 01:26:54 -0700

    first commit

---
+++ jarvis.py
@@ -0,0 +1,34 @@
+from gtts import gTTs
+import speech_recognition as sr
+import os
+import webbrowser
+import smtplib
+
+def talkToMe(audio):
+    print(audio)
+    tts = gTTs(text=audio, languae='en')
+    tts.save('audio.mp3')
+    os.system('mpg123 audio.mp3')
+
+# Listen for commands
+
+def myCommand(parameter_list):
+    
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('I am ready for your next command master')  
+        r.pause_threshold =1
+        r.adjust_for_ambient_noise(source, duration = 1)
+        audio = r.listen(source)
+
+        try:
+            command = r.recognize_google(audio)
+            print('You said: ' + command + '/n')
+        
+        # Loop back to continue listening for commands
+
+        except sr.UnknownValueError:
+            assistant(myCommand())
+
+        return command    
